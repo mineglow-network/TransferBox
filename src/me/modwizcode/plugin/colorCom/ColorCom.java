9b622e446d0704e0f1841f602c70e32bd28f3b59
Starbuck Johnson <Starbuck Johnson@tweetlebeetles>
2012-05-25 20:34:53 -0500

    Initial commit of colored chat plugin.

---
+++ src/me/modwizcode/plugin/colorCom/ColorCom.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.modwizcode.plugin.colorCom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Starbuck Johnson
+ */
+public class ColorCom extends JavaPlugin{
+    public void onEnable() {
+        
+    }
+    
+    public void onDisable() {
+        
+    }
+}
