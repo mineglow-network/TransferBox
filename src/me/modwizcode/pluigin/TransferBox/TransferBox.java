4ad1f12bc36e6917653eec2668aaaba081948b9e
Starbuck Johnson <Starbuck Johnson@sdf-modwizcode>
2012-05-26 11:53:52 -0500

    Initial commit.

---
+++ src/me/modwizcode/pluigin/TransferBox/TransferBox.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.modwizcode.pluigin.TransferBox;
+
+/**
+ *
+ * @author Starbuck Johnson
+ */
+public class TransferBox {
+    
+}
