a3cc3f4c386bd1450b34f857219d2542dee6bf56
Starbuck Johnson <Starbuck Johnson@sdf-modwizcode>
2012-05-26 19:22:17 -0500

    Initial Commit.

--- src/me/modwizcode/plugin/TransferBox/TransferBox.java
+++ src/me/modwizcode/plugin/TransferBox/TransferBox.java
@@ -4,18 +4,30 @@
  */
 package me.modwizcode.plugin.TransferBox;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
  * @author Starbuck Johnson
  */
-public class TransferBox extends JavaPlugin{
+public class TransferBox extends JavaPlugin implements Listener{
+    
+    TransferCommands tc;
+    @Override
     public void onEnable() {
-        
+        tc = new TransferCommands(this);
+        this.getPluginLoader().createRegisteredListeners(this, new TransferListener());
     }
     
     public void onDiable() {
         
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return tc.executeCommand(sender,command,label,args);
     }
 }
